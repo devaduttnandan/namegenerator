bb63fce89fb43cca482b45f13fa2924e2cda98d6
root <root@LAPTOP-901LLDEI.localdomain>
2025-04-03 16:07:32 +0530

    namegenerator

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+ required_version = ">= 1.0.7"  
+   backend "remote" {
+    organization = "organizationA12"
+    workspaces {
+      name = "namegenerator"
+    }
+  }
+}
+variable "name_length" {
+  description = "The number of words in the pet name"
+  default     = "9"
+}
+resource "random_pet" "pet_name" {
+  length    = var.name_length
+  separator = "-"
+}
+output "pet_name" {
+  value = random_pet.pet_name.id
+}
